ec5bc97fa2e05620d7022fe5b4b36ef126a0352b
Pr1deCIS <50147966+Pr1deCIS@users.noreply.github.com>
2019-05-13 04:11:42 +0300

    Add files via upload

---
+++ hw3_fucn.java
@@ -0,0 +1,31 @@
+import java.util.Scanner;
+
+public class hw3_fucn {
+    public static void main(String[] args) {
+        double a, b, c, D;
+        double d1, d2, d3;
+        Scanner in = new Scanner(System.in);
+        a = in.nextDouble();
+        b = in.nextDouble();
+        c = in.nextDouble();
+        D = b * b - 4 * a * c;
+        d1 = -b;
+        d2 = Math.sqrt(D);
+        d3 = 2 * a;
+
+        if (D > 0) {
+            double x1, x2;
+            x1 = (d1 + d2) / d3;
+            x2 = (d1 - d2) / d3;
+            System.out.println("Корни уравнения : " + x1 + " " + x2);
+        }
+        if (D < 0) {
+            System.out.println("Нет корней");
+        }
+        if (D == 0) {
+            double x1;
+            x1 = d1 / d3;
+            System.out.println("Дискрименант равен нулю, единственный корень : " + x1);
+        }
+    }
+}
