78fffe4b4482a98bca0e0b3896c3c4d7e481e976
Pr1deCIS <50147966+Pr1deCIS@users.noreply.github.com>
2019-05-13 04:56:57 +0300

    Add files via upload

---
+++ hw1_iter.java
@@ -0,0 +1,21 @@
+import java.util.Scanner;
+
+public class hw1_iter {
+    public static void main(String[] args) {
+        int a, b;
+        b = 0;
+
+        Scanner In = new Scanner(System.in);
+        a = In.nextInt();
+        for (int i = a; i > 0; i /=10) {
+            b += i % 10;
+        }
+        System.out.println(b);
+
+    }
+}
+
+
+
+
+
